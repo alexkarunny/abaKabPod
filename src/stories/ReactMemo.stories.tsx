10c5f7cefa52bf91bfb85ffb8c91187ea43ded67
alexkarunny <smetan4ik1@gmail.com>
2021-08-18 15:20:29 +0300

    Lesson 16. HOC React.memo

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+
+export default {
+    title: 'HOC react.memo demo'
+}
+
+const NewMessageCounter = (props: {count: number}) => {
+    return <div>{props.count}</div>
+}
+
+const UsersSecter = (props: {users: Array<string>}) => {
+    return <div>{
+        props.users.map((u, i) => <div key={i}>{u}</div>)
+    }</div>
+}
+
+const Users = React.memo(UsersSecter)
+
+export const Example = () => {
+    const [counter, setCounter] = useState(0);
+    const [users, setUsers] = useState(["Dymich", "Valera", "Alex"])
+    const addUser = () => {
+        const newUsers = [...users, `${'sad'} new Date()`]
+        setUsers(newUsers)
+    }
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={addUser}>add user</button>
+        <NewMessageCounter count={counter} />
+        <Users users={users} />
+    </>
+}
