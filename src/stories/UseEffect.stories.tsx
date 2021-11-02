0ca17186821db8280ea6d1e9c375208a07733905
alexkarunny <smetan4ik1@gmail.com>
2021-11-02 21:33:47 +0300

    Lesson 20, useEffect

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from "react";
+
+export default {
+    title: 'useEffect',
+}
+
+export const Example = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1);
+
+    useEffect(() => {
+        console.log('every time')
+        document.title = counter.toString()
+    })
+
+    useEffect(() => {
+        console.log('once')
+        document.title = counter.toString()
+    }, [])
+
+    useEffect(() => {
+        console.log('then dependence is changed')
+        document.title = counter.toString()
+    }, [fake])
+
+    return <>
+        <button onClick={() => setCounter(state => state + 1)}>counter+</button>
+        <button onClick={() => setFake(state => state + 1)}>fake+</button>
+        <div>counter: {counter}</div>
+        <div>fake: {fake}</div>
+    </>
+}
